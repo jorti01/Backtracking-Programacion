22f5689f11d98880512ea8b9b06f4ba19103604e
jorti01 <56034091+jorti01@users.noreply.github.com>
2019-10-20 19:01:51 -0500

    CV2 Implementación

--- EntregaParcial.py
+++ EntregaParcial.py
@@ -4,16 +4,29 @@
 """
 
 import turtle
+import cv2
+import numpy as np
+
+
+
+
+
+img = cv2.imread("D:/maze.jpg", 0)
+print(img)
+
+
+laberinto = []
+
+
+
+
+dibujaLaberinto = turtle.Turtle() ## Tortuga usada para cualquier funcion que tenga relación con el dibujo del laberinto base.
+
 
 ## Para dibujar el laberinto se requiere de una función que dibuje cajas por cada obstaculo representado por 1 en Binario
 
-dibujaLaberinto = turtle.Turtle()
 def caja(tortuga,tamaño):
     tortuga.begin
     for a in range(5):
         tortuga.forward(tamaño)
         tortuga.right(360/4)
-        
-    
-        
-caja(dibujaLaberinto,40)
